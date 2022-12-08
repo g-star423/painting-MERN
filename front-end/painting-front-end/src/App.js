f58aa9eb44158934330edb82884f53a8d53b4e3f
Gregory Shoup <gregshoup@gmail.com>
2022-12-08 17:57:40 -0500

    full CRUD working - time for styling updates

--- front-end/painting-front-end/src/App.js
+++ front-end/painting-front-end/src/App.js
@@ -11,19 +11,41 @@ function App() {
   //////////////////////////////////////
   const [allPaintings, setAllPaintings] = useState([])
 
-
-  // will use when we update
-  // const [updatePainting, setUpdatePainting] = useState('')
-  // const [updateArtist, setUpdateArtist] = useState('')
-  // const [updateYear, setUpdateYear] = useState('')
-  // const [updateStyle, setUpdateStyle] = useState('')
-  // const [updateImage, setUpdateImage] = useState('')
+  const [updatePainting, setUpdatePaintingName] = useState('')
+  const [updateArtist, setUpdateArtist] = useState('')
+  const [updateYear, setUpdateYear] = useState('')
+  const [updateStyle, setUpdateStyle] = useState('')
+  const [updateImage, setUpdateImage] = useState('')
+  const [updateID, setUpdateID] = useState('')
 
 
   //////////////////////////////////////
   //event handlers
   //////////////////////////////////////
+  const handleUpdatePaintingChange = (event) => {
+    setUpdatePaintingName(event.target.value)
+  }
+  const handleUpdateArtistChange = (event) => {
+    setUpdateArtist(event.target.value)
+  }
+  const handleUpdateYearChange = (event) => {
+    setUpdateYear(event.target.value)
+  }
+  const handleUpdateStyleChange = (event) => {
+    setUpdateStyle(event.target.value)
+  }
+  const handleUpdateImageChange = (event) => {
+    setUpdateImage(event.target.value)
+  }
 
+  const handleUpdateInput = (painting) => {
+    setUpdateArtist(painting.artist)
+    setUpdateID(painting._id)
+    setUpdateImage(painting.image)
+    setUpdatePaintingName(painting.name)
+    setUpdateStyle(painting.style)
+    setUpdateYear(painting.year)
+  }
 
   //////////////////////////////////////
   //Axios requests
@@ -39,6 +61,26 @@ function App() {
     })
   }
 
+  const handlePaintingDelete = (paintingID) => {
+    axios.delete('http://localhost:3001/paintings/' + paintingID).then((response) => {
+      updateAllPaintings()
+    })
+  }
+
+  const handlePaintingUpdateSubmit = (event, paintingID) => {
+    event.preventDefault()
+    axios.put('http://localhost:3001/paintings/' + paintingID,
+      {
+        name: updatePainting,
+        artist: updateArtist,
+        year: updateYear,
+        style: updateStyle,
+        image: updateImage
+      }).then((response) => {
+        updateAllPaintings()
+      })
+  }
+
 
 
   //////////////////////////////////////
@@ -51,16 +93,23 @@ function App() {
       <h3>Add a New Painting</h3>
       <NewPainting updateAllPaintings={updateAllPaintings}></NewPainting>
 
-      <div className='cardContainer'>
+      <div className='card-container'>
         {allPaintings.map((painting) => {
           return (
-            <PaintingCard painting={painting}></PaintingCard>
+            <PaintingCard key={painting._id} painting={painting} handlePaintingDelete={handlePaintingDelete} handleUpdateInput={handleUpdateInput}></PaintingCard>
           )
         })}
       </div>
-
-
-
+      <div className='update-container'>
+        <form onSubmit={(event) => handlePaintingUpdateSubmit(event, updateID)}>
+          Painting Name:<input type="text" value={updatePainting} onChange={handleUpdatePaintingChange} /><br />
+          Artist:<input type="text" value={updateArtist} onChange={handleUpdateArtistChange} /><br />
+          Year:<input type="number" value={updateYear} onChange={handleUpdateYearChange} /><br />
+          Style:<input type="text" value={updateStyle} onChange={handleUpdateStyleChange} /><br />
+          Image URL:<input type="text" value={updateImage} onChange={handleUpdateImageChange} /><br />
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 }
--- front-end/painting-front-end/src/components/PaintingCard.js
+++ front-end/painting-front-end/src/components/PaintingCard.js
@@ -6,9 +6,13 @@ const PaintingCard = (props) => {
 
     return (
         <div className="card">
-            <h3>Painting Name: {props.painting.name}</h3>
+            <h3>{props.painting.name}</h3>
             <img src={props.painting.image} />
-            <p>Painter: {props.painting.painter}</p>
+            <p>Painter: {props.painting.artist}</p>
+            <p>Year Painted: {props.painting.year}</p>
+            <p>Style: {props.painting.style}</p>
+            <button onClick={() => props.handlePaintingDelete(props.painting._id)}>Delete Painting</button>
+            <button onClick={() => props.handleUpdateInput(props.painting)}>Edit Painting</button>
         </div>
     )
 }
