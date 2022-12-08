f9c7c958de7e11650913c5dd5f006f67247b8a7a
Gregory Shoup <gregshoup@gmail.com>
2022-12-08 11:11:27 -0500

    created schema

---
+++ back-end/models/paintings.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const paintingSchema = new mongoose.Schema({
+    name: String,
+    artist: String,
+    year: Number,
+    style: String,
+    image: String
+})
+
+const Painting = mongoose.model('Painting', paintingSchema)
+
+module.exports = Painting
